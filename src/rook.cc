738d273d4b5f88c32af94d6f99b02c030e9f8d3c
Alishba505 <90855799+Alishba505@users.noreply.github.com>
2024-07-22 16:47:18 -0400

    added isvalid to rook

--- src/rook.cc
+++ src/rook.cc
@@ -5,13 +5,13 @@ Rook::Rook(Colour colour, char type, Chessboard *board, char cPos, int iPos) : P
 }
 
 bool Rook::isValidMove(char newC, int newI) {
-    //make sure we're moving diagonally
+    //make sure we're moving horizontally or vertically
     if ((newC != cPos && newI != iPos) || (newC == cPos && newI == iPos)) {
         return false;
     }
 
-    int colStep = (newC > cPos) ? 1 : -1;
-    int rowStep = (newI > iPos) ? 1 : -1;
+    int colStep = (newC == cPos)? 0 : (newC > cPos) ? 1 : -1;
+    int rowStep = (newC == cPos)? 0 : (newI > iPos) ? 1 : -1;
 
     char currentCol = cPos;
     int currentRow = iPos;
@@ -29,5 +29,6 @@ bool Rook::isValidMove(char newC, int newI) {
 }
 
 vector<tuple<int, int>> generateMves() {
-    
+     // keep i pos the same and go through a-h for cpos and call is valid move
+    //keep cpos the same and go though 1-8 for ipos and call is calid move
 }
