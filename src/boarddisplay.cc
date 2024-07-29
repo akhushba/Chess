6d945c3a50a501a1ccbf6d516d97da2c488efea7
Amna Khushbakht <113641076+akhushba@users.noreply.github.com>
2024-07-29 15:26:56 -0400

    update pieceSet

--- src/boarddisplay.cc
+++ src/boarddisplay.cc
@@ -138,6 +138,7 @@ void BoardDisplay::addPiece(char type, const std::string pos) {
     PlayerInfo* currentPlayer = (c == BLACK) ? blackPlayer : whitePlayer;
     setState(newPiece, cPos, iPos);
     currentPlayer->activePieces.push_back(newPiece);
+    currentPlayer->player->pieceSet->push_back(newPiece);
 }
 
 
